e5fbafac6109f3d361abc625998dc1327d551520
Stuart Thomson <stu.thom.96@gmail.com>
2018-09-23 21:01:01 +1200

    Add files for colour picker slider thingy

---
+++ src/components/ColorPickerSliders/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as chroma from 'chroma-js';
+import autobind from 'autobind-decorator';
+import './index.css';
+
+export default class RgbColorSliders extends React.Component {
+
+}
