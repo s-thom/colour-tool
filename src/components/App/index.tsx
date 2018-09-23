9e96cbc5da3a8ed10990b1a19a2e8a023e564e32
Stuart Thomson <stu.thom.96@gmail.com>
2018-09-23 21:21:07 +1200

    Added component to wrap the colour picking stuff
    Probably going to be renamed

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 import * as chroma from 'chroma-js';
 import './index.css';
 
-import logo from './logo.svg';
-import GradientSlider from '../GradientSlider';
+import ColorPickerApp from '../ColorPickerApp';
 
 class App extends React.Component {
   render() {
@@ -12,21 +11,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <GradientSlider
-          startColor={start}
-          endColor={end}
-          startNumeric={0}
-          endNumeric={255}
-          current={36}
-          onValueChanged={(v) => console.log('is', v)}
-        />
+        <ColorPickerApp />
       </div>
     );
   }
---
+++ src/components/ColorPickerApp/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import * as chroma from 'chroma-js';
+import autobind from 'autobind-decorator';
+import './index.css';
+import ColorPickerSliders from '../ColorPickerSliders';
+
+interface IColorPickerAppProps {
+  temp?: false;
+}
+
+interface IColorPickerAppState {
+  current: chroma.Color;
+}
+
+export default class ColorPickerApp extends React.Component<IColorPickerAppProps, IColorPickerAppState> {
+  constructor(props: IColorPickerAppProps) {
+    super(props);
+
+    this.state = {
+      current: chroma.random(),
+    };
+  }
+
+  @autobind
+  onColorChange(color: chroma.Color) {
+    this.setState((prevState) => ({
+      ...prevState,
+      current: color,
+    }));
+  }
+
+  render() {
+    const {
+      current,
+    } = this.state;
+
+    return (
+      <ColorPickerSliders
+        color={current}
+        onColorChange={this.onColorChange}
+      />
+    );
+  }
+}
