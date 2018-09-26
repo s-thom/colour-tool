3988e833d32d06e9af21d912a7d6eff40db94618
Stuart Thomson <stu.thom.96@gmail.com>
2018-09-26 19:47:34 +1200

    Created SwatchList component
    Just to abstract that out a little bit

---
+++ src/components/SwatchList/index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import * as chroma from 'chroma-js';
+import './index.css';
+import Swatch from '../Swatch';
+import autobind from 'autobind-decorator';
+
+interface ISwatchListProps {
+  colors: chroma.Color[];
+  showHex?: boolean;
+  onColorSelected?: (color: chroma.Color) => void;
+}
+
+export default class SwatchList extends React.Component<ISwatchListProps> {
+
+  @autobind
+  onSwatchClicked(color: chroma.Color) {
+    if (this.props.onColorSelected) {
+      this.props.onColorSelected(color);
+    }
+  }
+
+  render() {
+    const {
+      colors,
+      showHex,
+    } = this.props;
+
+    return (
+      <div className="SwatchList">
+        {colors.map(c => (
+          <Swatch
+            key={c.hex()}
+            color={c}
+            showHex={showHex}
+            onClick={() => this.onSwatchClicked(c)}
+          />
+        ))}
+      </div>
+    );
+  }
+}
