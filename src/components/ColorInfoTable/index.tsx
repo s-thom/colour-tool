0321f855ba4f62ae33273f7d1c49885766b16fca
Stuart Thomson <stu.thom.96@gmail.com>
2018-09-26 18:50:14 +1200

    Create class for a row of the colour table

--- src/components/ColorInfoTable/index.tsx
+++ src/components/ColorInfoTable/index.tsx
@@ -4,6 +4,46 @@ import autobind from 'autobind-decorator';
 import './index.css';
 import ColorSpan from '../ColorSpan';
 
+interface IColorPart {
+  color: chroma.Color;
+  name: string;
+  text: string;
+}
+
+interface IColorInfoTableRowProps {
+  color: chroma.Color;
+  parts: IColorPart[];
+  stringified?: string;
+  title: string;
+}
+
+class ColorInfoTableRow extends React.Component<IColorInfoTableRowProps> {
+  render() {
+    const {
+      color,
+      parts,
+      stringified,
+      title,
+    } = this.props;
+
+    const partElements = parts.map(p => (
+      <td key={p.name}>
+        {p.name}: <ColorSpan color={p.color}>{p.text}</ColorSpan>
+      </td>
+    ));
+
+    return (
+      <tr>
+        <td>{title}:</td>
+        {partElements}
+        {stringified && (
+          <td><ColorSpan color={color}>{stringified}</ColorSpan></td>
+        )}
+      </tr>
+    );
+  }
+}
+
 interface IColorInfoTableProps {
   color: chroma.Color;
 }
@@ -15,139 +55,186 @@ export default class ColorInfoTable extends React.Component<IColorInfoTableProps
       color,
     } = this.props;
 
+    // RGB(A)
     const rgbArr = color.rgba();
+    const hexParts: IColorPart[] = [
+      {
+        color: chroma.rgb(rgbArr[0], 0, 0),
+        name: 'R',
+        text: rgbArr[0].toString(16),
+      },
+      {
+        color: chroma.rgb(0, rgbArr[1], 0),
+        name: 'G',
+        text: rgbArr[1].toString(16),
+      },
+      {
+        color: chroma.rgb(0, 0, rgbArr[2]),
+        name: 'B',
+        text: rgbArr[2].toString(16),
+      },
+    ];
+    const rgbParts: IColorPart[] = [
+      {
+        color: chroma.rgb(rgbArr[0], 0, 0),
+        name: 'R',
+        text: rgbArr[0].toString(),
+      },
+      {
+        color: chroma.rgb(0, rgbArr[1], 0),
+        name: 'G',
+        text: rgbArr[1].toString(),
+      },
+      {
+        color: chroma.rgb(0, 0, rgbArr[2]),
+        name: 'B',
+        text: rgbArr[2].toString(),
+      },
+    ];
+    // HSV/HSL
     const hslArr = color.hsl();
     const hsvArr = color.hsv();
-    const labArr = color.lab();
-    const hclArr = color.hcl();
-    const cmykArr = color.cmyk();
-
-    // RGB(A)
-    const redColor = chroma.rgb(rgbArr[0], 0, 0);
-    const greenColor = chroma.rgb(0, rgbArr[1], 0);
-    const blueColor = chroma.rgb(0, 0, rgbArr[2]);
-    const alphaColor = chroma('white').alpha(rgbArr[3]);
-    // HSV/HSL
-    const hueColor = chroma.hsl(hslArr[0] || 0, 1, 0.5);
-    const satColor = chroma.hsl(0, 0, hslArr[1]);
-    const lumColor = chroma.hsl(0, 0, hslArr[2]);
-    const sat2Color = chroma.hsv(0, 0, hsvArr[1]);
-    const valColor = chroma.hsv(0, 0, hsvArr[2]);
+    const hslParts: IColorPart[] = [
+      {
+        color: chroma.hsl(hslArr[0] || 0, 1, 0.5),
+        name: 'H',
+        text: (hslArr[0] || 0).toFixed(2),
+      },
+      {
+        color: chroma.hsl(0, 0, hslArr[1]),
+        name: 'S',
+        text: (hsvArr[1]).toFixed(2),
+      },
+      {
+        color: chroma.hsl(0, 0, hslArr[2]),
+        name: 'L',
+        text: (hsvArr[2]).toFixed(2),
+      },
+    ];
+    const hsvParts: IColorPart[] = [
+      {
+        color: chroma.hsl(hsvArr[0] || 0, 1, 0.5),
+        name: 'H',
+        text: (hsvArr[0] || 0).toFixed(2),
+      },
+      {
+        color: chroma.hsv(0, 0, hsvArr[1]),
+        name: 'S',
+        text: hsvArr[1].toFixed(2),
+      },
+      {
+        color: chroma.hsv(0, 0, hsvArr[2]),
+        name: 'V',
+        text: hsvArr[2].toFixed(2),
+      },
+    ];
     // Lab
-    const LColor = chroma.lab(labArr[0], 0, 0);
-    const aColor = chroma.lab(50, labArr[1], 0);
-    const bColor = chroma.lab(50, 0, labArr[2]);
+    const labArr = color.lab();
+    const labParts: IColorPart[] = [
+      {
+        color: chroma.lab(labArr[0], 0, 0),
+        name: 'L',
+        text: (labArr[0]).toFixed(2),
+      },
+      {
+        color: chroma.lab(50, labArr[1], 0),
+        name: 'a',
+        text: (labArr[1]).toFixed(2),
+      },
+      {
+        color: chroma.lab(50, 0, labArr[2]),
+        name: 'b',
+        text: (labArr[2]).toFixed(2),
+      },
+    ];
     // HCL
-    const hue2Color = chroma.lch(50, 50, hclArr[0]);
-    const chromaColor = chroma.lch(50, hclArr[1], hclArr[0]);
-    const ligColor = chroma.lch(hclArr[2], 0, 0);
+    const hclArr = color.hcl();
+    const hclParts: IColorPart[] = [
+      {
+        color: chroma.lch(50, 50, hclArr[0]),
+        name: 'H',
+        text: (hclArr[0]).toFixed(2),
+      },
+      {
+        color: chroma.lch(50, hclArr[1], hclArr[0]),
+        name: 'C',
+        text: (hclArr[1]).toFixed(2),
+      },
+      {
+        color: chroma.lab(50, 0, labArr[2]),
+        name: 'L',
+        text: (hclArr[2]).toFixed(2),
+      },
+    ];
     // CMYK
-    const cColor = chroma.cmyk(cmykArr[0], 0, 0, 0);
-    const mColor = chroma.cmyk(0, cmykArr[1], 0, 0);
-    const yColor = chroma.cmyk(0, 0, cmykArr[2], 0);
-    const kColor = chroma.cmyk(0, 0, 0, cmykArr[3]);
+    const cmykArr = color.cmyk();
+    const cmykParts: IColorPart[] = [
+      {
+        color: chroma.cmyk(cmykArr[0], 0, 0, 0),
+        name: 'C',
+        text: (cmykArr[0]).toFixed(2),
+      },
+      {
+        color: chroma.cmyk(0, cmykArr[1], 0, 0),
+        name: 'M',
+        text: (cmykArr[1]).toFixed(2),
+      },
+      {
+        color: chroma.cmyk(0, 0, cmykArr[2], 0),
+        name: 'Y',
+        text: (cmykArr[2]).toFixed(2),
+      },
+      {
+        color: chroma.cmyk(0, 0, 0, cmykArr[3]),
+        name: 'K',
+        text: (cmykArr[3]).toFixed(2),
+      },
+    ];
 
     return (
       <div className="ColorInfo">
         <div className="ColorInfo-table-container">
           <table className="ColorInfo-table">
-            <tr>
-              <td>Hex:</td>
-              <td>
-                R: <ColorSpan color={redColor}>{rgbArr[0].toString(16)}</ColorSpan>
-              </td>
-              <td>
-                G: <ColorSpan color={greenColor}>{rgbArr[1].toString(16)}</ColorSpan>
-              </td>
-              <td>
-                B: <ColorSpan color={blueColor}>{rgbArr[2].toString(16)}</ColorSpan>
-              </td>
-              <td>
-                <ColorSpan color={color}>{color.hex()}</ColorSpan>, int <ColorSpan color={color}>{(color as any).num()}</ColorSpan>
-              </td>
-            </tr>
-            <tr>
-              <td>RGB:</td>
-              <td>
-                R: <ColorSpan color={redColor}>{rgbArr[0]}</ColorSpan>
-              </td>
-              <td>
-                G: <ColorSpan color={greenColor}>{rgbArr[1]}</ColorSpan>
-              </td>
-              <td>
-                B: <ColorSpan color={blueColor}>{rgbArr[2]}</ColorSpan>
-              </td>
-              <td><ColorSpan color={color}>{`rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`}</ColorSpan></td>
-            </tr>
-            <tr>
-              <td>HSL:</td>
-              <td>
-                H: <ColorSpan color={hueColor}>{(hslArr[0] || 0).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                S: <ColorSpan color={satColor}>{(hslArr[1]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                L: <ColorSpan color={lumColor}>{(hslArr[2]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                <ColorSpan color={color}>{color.css('hsl')}</ColorSpan>
-              </td>
-            </tr>
-            <tr>
-              <td>HSV:</td>
-              <td>
-                H: <ColorSpan color={hueColor}>{(hsvArr[0] || 0).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                S: <ColorSpan color={sat2Color}>{(hsvArr[1]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                V: <ColorSpan color={valColor}>{(hsvArr[2]).toFixed(2)}</ColorSpan>
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Lab:</td>
-              <td>
-                L: <ColorSpan color={LColor}>{(labArr[0]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                a: <ColorSpan color={aColor}>{(labArr[1]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                b: <ColorSpan color={bColor}>{(labArr[2]).toFixed(2)}</ColorSpan>
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>HCL/LCh:</td>
-              <td>
-                H: <ColorSpan color={hue2Color}>{(hclArr[0]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                C: <ColorSpan color={chromaColor}>{(hclArr[1]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                L: <ColorSpan color={ligColor}>{(hclArr[2]).toFixed(2)}</ColorSpan>
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>CMYK:</td>
-              <td>
-                C: <ColorSpan color={cColor}>{(cmykArr[0]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                M: <ColorSpan color={mColor}>{(cmykArr[1]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                Y: <ColorSpan color={yColor}>{(cmykArr[2]).toFixed(2)}</ColorSpan>
-              </td>
-              <td>
-                K: <ColorSpan color={kColor}>{(cmykArr[3]).toFixed(2)}</ColorSpan>
-              </td>
-            </tr>
+            <tbody>
+              <ColorInfoTableRow
+                color={color}
+                parts={hexParts}
+                stringified={color.hex()}
+                title="Hex"
+              />
+              <ColorInfoTableRow
+                color={color}
+                parts={rgbParts}
+                stringified={color.css()}
+                title="RGB"
+              />
+              <ColorInfoTableRow
+                color={color}
+                parts={hslParts}
+                stringified={color.css('hsl')}
+                title="HSL"
+              />
+              <ColorInfoTableRow
+                color={color}
+                parts={hsvParts}
+                title="HSV"
+              />
+              <ColorInfoTableRow
+                color={color}
+                parts={labParts}
+                title="Lab"
+              />
+              <ColorInfoTableRow
+                color={color}
+                parts={hclParts}
+                title="HCL"
+              />
+              <ColorInfoTableRow
+                color={color}
+                parts={cmykParts}
+                title="CMYK"
+              />
+            </tbody>
           </table>
         </div>
       </div>
