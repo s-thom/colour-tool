20c9e659bd6cb66fff9dca121630d3aaf96aa236
Stuart Thomson <stu.thom.96@gmail.com>
2018-09-30 09:32:43 +1300

    Fixed background colour coverage

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -10,19 +10,23 @@ import Route404 from '../Route404';
 import RouteColor from '../RouteColor';
 import RouteRandomColor from '../RouteRandomColor';
 import RouteGradient from '../RouteGradient';
+import Navigation from '../Navigation';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Switch>
-            <Route path="/" exact component={RouteRandomColor} />
-            <Route path="/random" exact component={RouteRandomColor} />
-            <Route path="/gradient" component={RouteGradient} />
-            <Route path="/:color([A-Fa-f0-9]{6,6})" component={RouteColor} />
-            <Route component={Route404} />
-          </Switch>
+          <Navigation/>
+          <div className="App-area">
+            <Switch>
+              <Route path="/" exact component={RouteRandomColor} />
+              <Route path="/random" exact component={RouteRandomColor} />
+              <Route path="/gradient" component={RouteGradient} />
+              <Route path="/:color([A-Fa-f0-9]{6,6})" component={RouteColor} />
+              <Route component={Route404} />
+            </Switch>
+          </div>
         </div>
       </Router>
     );
