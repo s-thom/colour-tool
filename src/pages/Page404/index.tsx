843bcbaa3ca4dfe3d7c66761e9d5e7c4c699265a
Stuart Thomson <stu.thom.96@gmail.com>
2018-09-30 14:09:25 +1300

    Add some more random colours for the 404 page

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -3,18 +3,31 @@ import * as chroma from 'chroma-js';
 import Card from '../../components/Card';
 import Swatch from '../../components/Swatch';
 import './index.css';
+import { arrayRandom } from '../../util';
 
-const color404 = chroma.hex('#440044');
+const color440044 = chroma.hex('#440044');
+const color404404 = chroma.hex('#404404');
+const color400004 = chroma.hex('#400004');
+const color404000 = chroma.hex('#404000');
+// const color000404 = chroma.hex('#000404');
+const colorList = [
+  color404404,
+  color440044,
+  color400004,
+  color404000,
+  // color000404,
+];
 
 export default class Page404 extends React.Component {
   render() {
+    const color = arrayRandom(colorList);
 
     return (
       <div className="Page404">
         <div
           className="Page404-background"
           style={{
-            backgroundColor: color404.css(),
+            backgroundColor: color.css(),
           }}
         />
         <div className="Page404-content">
@@ -23,7 +36,7 @@ export default class Page404 extends React.Component {
             <h2>Page not found</h2>
             <div className="Page404-404-container">
               <Swatch
-                color={color404}
+                color={color}
                 showHex={true}
               />
             </div>
